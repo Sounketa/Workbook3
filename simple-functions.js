8eee57bf21de728be1efdb669962ae8974fcb63a
Sounketa <julien.ilboudo97@gmail.com>
2024-04-29 15:11:36 -0400

    function1

---
+++ simple-functions.js
@@ -0,0 +1,21 @@
+function favoriteThings() {
+    console.log("Going fishing is fun");
+    console.log("My fist tank and pet Prometheus");
+    console.log("Going to the gym");
+    console.log("Just relaxing is fun also");
+}
+favoriteThings();
+
+function whyImHere(params) {
+    console.log("Because I want to learn and develop my skills!");
+    console.log("Make new friends and business relationships");
+    console.log("Start my career in Tech");
+}
+whyImHere();
+
+function favoritePlace(){
+    console.log("Home will always be my fsvorite place to be");
+    console.log("The gym is becoming my favorite place");
+   console.log("Montauk,Long Island");
+}
+favoritePlace();
