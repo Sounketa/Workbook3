636b80f03e83ab75b46397bf129bfd1eb477ec1d
Sounketa <julien.ilboudo97@gmail.com>
2024-05-01 14:46:36 -0400

    more date examples; birthday

---
+++ excercises/dates/date-conversions.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let birthday = new Date(1997, 0, 16);
+console.log(birthday.toLocaleString());
+
+let mombirthday = new Date(1976, 0, 2);
+console.log(mombirthday.toLocaleString());
+
+let dadbirthday = new Date(1957, 8, 3);
+console.log(dadbirthday.toLocaleString());
