1459b5de74d241e66feceed3ff93758ec1392841
Sounketa <julien.ilboudo97@gmail.com>
2024-05-01 11:34:15 -0400

    parsing

---
+++ excercises/strings/parsing.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let name = "Julien Ilboudo";
+let posOfFirstSpace = name.indexOf(" ")
+console.log(posOfFirstSpace);
+
+let first = name.substring(0, posOfFirstSpace);
+console.log(first);
+
+let last = name.substring(posOfFirstSpace + 1);
+console.log(last);
+
+function parseAndDisplayName(name) {
+    
+}
+
+console.log(name);
